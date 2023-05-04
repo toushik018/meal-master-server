b0700fdcf2fa7046d2785fd149d53d93b00dd6d4
Toushik <tayebhossain018@gmail.com>
2023-05-04 18:20:46 +0600

    vercel hosting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 const chefs = require('./data/cardData.json')
 
+
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -14,6 +15,20 @@ res.send('Meal master server')
 app.get('/chefs', (req, res) =>{
     res.send(chefs);
 })
+
+app.get('/chefs/:id', (req, res) => {
+    const chefId = req.params.id;
+    const selectedChef = chefs.chefs.find(chef => chef.id === Number(chefId));
+
+    if (selectedChef) {
+        res.send(selectedChef);
+    } else {
+        res.status(404).send('Chef Not found');
+    }
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`The meal master API is running on port: ${port}`);
